817a25c2b57cc28c27f0ad89a469b331cd50ddda
cmzluna <claudiomazzoli@gmail.com>
2023-02-19 17:47:43 -0300

    docker compose-up setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
